b793d25f2345c14821c84a1ea26dc3ee705cc55e
Alison <agale753@gmail.com>
2020-05-20 09:29:00 -0700

    Update nav bar to include the knockout round

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {path: 'livestream', component: LivestreamComponent},
-    // {path: 'matchup', component: MatchupComponent},
     {path: 'prelims', component: PrelimsComponent},
     {path: 'knockout', component: HomeComponent},
-    // TODO(agale): Redirect to knockout when that launches.
-    {path: '**', redirectTo: 'prelims'}
+    {path: '**', redirectTo: 'knockout'}
 ];
 
 @NgModule({
